d828b42d9dec614e9bec09e7c51bf0f871278143
darkasmist-hub <darkas.mist@gmail.com>
2026-01-06 13:33:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const bar = document.querySelector('#bar');
-const menu = document.querySelector('#menu');
-
-if( bar ){
-bar.addEventListener('click',()=>{
-  menu.classList.toggle('active');
-});
-}
